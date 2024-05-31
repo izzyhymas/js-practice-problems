562eaa8ddda7b686c51126a3342020a5aed6975b
Izzy Hymas <izzyhymas@gmail.com>
2024-05-31 10:55:59 -0600

    Add more JS functions and styling

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const reverseString = () => {
     const string = "Hello World!";
     const reversedString = string.split("").reverse().join("");
-    console.log(`Original: ${string} | Reversed: ${reversedString}`);
+    alert(`Original: ${string} | Reversed: ${reversedString}`);
+};
+
+const fizzbuzz = () => {
+    for (let i = 0; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("fizzbuzz");
+        }
+        else if(i % 3 == 0) {
+            console.log("fizz");
+        }
+        else {
+            console.log("buzz");
+        }
+    }
+};
+
+const dogAgeToHumanAge = () => {
+    dogAge = prompt("Enter a dog age: ");
+    convertAge = 16 * Math.log(dogAge) + 31;
+    alert(`Your dog is ${Math.round(convertAge)} in human years!`);
 };
